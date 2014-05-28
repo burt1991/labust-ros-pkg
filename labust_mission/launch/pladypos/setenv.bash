19f8b2ec89437bae6406555bf998afeb4b881651
Đula Nađ <dula.nad@gmail.com>
2014-05-28 15:19:46 +0200

    Corrections to the LDTravModel and DVL sensor handler.

--- labust_mission/launch/pladypos/setenv.bash
+++ labust_mission/launch/pladypos/setenv.bash
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #Vehicle variables
-export MODEL=`rospack find snippets`/data/models/pladypos.yaml
+export MODEL=`rospack find snippets`/data/models/pladypos_dvl.yaml
 export ALLOCATION_MODEL=`rospack find snippets`/data/allocations/x_horizontal.yaml
 
 #Input
 export JOYSTICK=/dev/input/js1
 
 #Simulation variables
-export IS_SIM=1
+export IS_SIM=0
 export USE_NOISE=0
 export USE_VISUALIZATION=1
 
 #Location for simulation or predefined position
-export LOCATION=labos
-export USE_LOCAL_FIX=1
+export LOCATION=sdot_yam
+export USE_LOCAL_FIX=0
 
 #Control configuration
 export USE_IDENTIFICATION=1
-export USE_MULTIMASTER=0
+export USE_MULTIMASTER=1
 export USE_MC=1
 
 #Frame description
@@ -29,6 +29,8 @@ export TF_PREFIX=pladypos
 export USE_USBL_MANAGER=0
 export USE_USBL=0
 
+#ROS variables
+ROS_LOG_DIR=/extern/ros_logs
 
 
 

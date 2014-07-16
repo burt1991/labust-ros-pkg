d460eddfa6e8a458ece948bb7b58a13f712dfbea
Đula Nađ <dula.nad@gmail.com>
2014-07-16 18:47:13 +0200

    Fixed the wrong package specification in the labust_gui setup.py.

--- labust_gui/setup.py
+++ labust_gui/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['labust_gui'],
+    packages=['control','emonitor','labust_rqt'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy', 'rqt_gui', 'rqt_gui_py'])
 

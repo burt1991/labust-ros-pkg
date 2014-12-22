05d494a7ea4ce09fd43ab2c63fdd685849fbf5ea
gibo1990 <filip.mandic@gmail.com>
2014-12-22 12:30:31 +0100

    Started work on EKF based extremum seeking controller

--- labust_control/src/esctoolkit/esc_ekf_grad.cpp
+++ labust_control/src/esctoolkit/esc_ekf_grad.cpp
@@ -1,152 +1,152 @@
 /*
- * perturb_esc_nd.cpp
+ * esc_classic.cpp
  *
- *  Created on: Aug 1, 2012
- *      Author: Berk Calli
- *      Organization: Delft Biorobotics Lab., Delft University of Technology
- *  	Contact info: b.calli@tudelft.nl, web: www.dbl.tudelft.nl
- *
- * Class for perturbation based extremum seeking control
- *
- * * References:
- * - K. B. Ariyur and M. Krstic, "Real-Time Optimization by Extremum-Seeking Control", Wiley, 2003.
- * - B. Calli, W. Caarls, P. Jonker, M. Wisse, "Comparison of Extremum Seeking Control Algorithms for Robotic Applications", IROS 2012.
- *
+ *  Created on: Dec 22, 2014
+ *      Author: Filip Mandic
  */
 
-#include "esc_perturb/perturb_esc_nd.h"
+#include <labust/control/esc/EscPerturbation.hpp>
+//#include <vector>
 
-PerturbESCND::PerturbESCND(){
-	sin_amp_ = 0;
-	sin_freq_ = 0;
-	corr_gain_ = 0;
-	high_pass_pole_ = 0;
-	low_pass_pole_ = 0;
-	comp_pole_ = 0;
-	comp_zero_ = 0;
-	period_ = 0;
-	state_initialized_ = false;
-	initialized_ = false;
-	old_vals_initialized_ = false;
-}
-PerturbESCND::PerturbESCND(double sin_amp, double sin_freq, double corr_gain, double high_pass_pole, double low_pass_pole, double comp_zero, double comp_pole, double period){
-	init(sin_amp, sin_freq, corr_gain, high_pass_pole, low_pass_pole, comp_zero, comp_pole, period);
-}
-void PerturbESCND::init(double sin_amp, double sin_freq, double corr_gain, double high_pass_pole, double low_pass_pole, double comp_zero, double comp_pole, double period){
-	sin_amp_ = sin_amp;
-	sin_freq_ = sin_freq;
-	corr_gain_ = corr_gain;
-	high_pass_pole_ = high_pass_pole;
-	low_pass_pole_ = low_pass_pole;
-	comp_pole_ = comp_pole;
-	comp_zero_ = comp_zero;
-	period_ = period;
-	obj_val_old_ = 0;
-	cycle_count_ = 0;
-	hpf_out_old_ = 0;
-	opt_dim_ = 0;
-	state_initialized_ = false;
-	old_vals_initialized_ = false;
-	initialized_ = true;
-}
+#include <ros/ros.h>
 
-std::vector<double>  PerturbESCND::step(std::vector<double> state, double obj_val){
+namespace labust{
+	namespace control{
+		namespace esc{
 
-	if(!initialized_){
-		fprintf(stderr,"The perturbation based ESC (1D) is not initialized... It will not be executed. \n");
-		return std::vector<double>();
-	}
 
-	if(!state_initialized_ && state.empty()){
-		fprintf(stderr,"The state value of the perturbation based ESC (1D) cannot be initialized: State vector is empty. The algorithm will not be executed. \n");
-		return std::vector<double>();
-	}
+			class EscClassic : public EscPerturbationBase<double> {
 
-	else if(!state_initialized_){
+			public:
 
-		opt_dim_ = (unsigned int)state.size();
-		lpf_out_old_.resize(opt_dim_);
-		signal_demodulated_old_.resize(opt_dim_);
-		comp_old_.resize(opt_dim_);
-		corr_signal_.resize(opt_dim_);
-		pos_ref_.resize(opt_dim_);
-		for (size_t i = 0; i<opt_dim_; i++){
-			pos_ref_[i] = state[i];
-			lpf_out_old_[i] = 0;
-			signal_demodulated_old_[i] = 0;
-			comp_old_[i] = 0;
-			corr_signal_[i] = 0;
+				/*** Control inputs */
+				//enum {u = 0, v, controlNum};
+
+				EscClassic();
+
+				~EscClassic();
+
+				 void initController();
+
+				 numericprecission preFiltering(numericprecission cost_signal);
+
+				 vector gradientEstimation(numericprecission cost_signal_filtered, vector additional_input);
+
+				 vector postFiltering(vector estimated_gradient);
+
+				 vector superimposePerturbation(vector control);
+
+				 //vector step(numericprecission cost_signal);
+				 /***
+				 * K - gain
+				 * A0 - perturbation amplitude
+				 */
+				vector gain_, sin_amp_, sin_freq_, corr_gain_, low_pass_pole_, comp_pole_, comp_zero_;
+				vector control_ref_, signal_demodulated_old_, lpf_out_old_,corr_signal_, phase_shift_,comp_old_;
+				/*** Controlled state */
+				vector state_;
+
+				numericprecission high_pass_pole_, hpf_out_old_, obj_val_old_;
+				numericprecission period_;
+			};
+
+			typedef EscClassic Base;
+
+			EscClassic::EscClassic():state_(vector::Zero(controlNum)),period_(0){
+
+			}
+
+			EscClassic::~EscClassic(){
+
+			}
+
+			void EscClassic::initController(){
+
+				lpf_out_old_.resize(controlNum);
+				signal_demodulated_old_.resize(controlNum);
+				comp_old_.resize(controlNum);
+				corr_signal_.resize(controlNum);
+				control_ref_.resize(controlNum);
+
+				lpf_out_old_.setZero();
+				control_ref_ = state_;
+				lpf_out_old_.setZero();
+				signal_demodulated_old_.setZero();
+				comp_old_.setZero();
+				corr_signal_.setZero();
+
+				phase_shift_.resize(controlNum);
+				phase_shift_[0] = 0;
+				for (size_t i = 1; i<controlNum; i++){
+					phase_shift_[i] = i*M_PI/((double)controlNum);
+				}
+				state_initialized_ = true;
+
+			}
+
+			Base::numericprecission EscClassic::preFiltering(numericprecission cost_signal){
+
+				return (-(period_*high_pass_pole_-2)*pre_filter_output_old_+2*cost_signal-2*pre_filter_input_old_)/(2+high_pass_pole_*period_);
+			}
+
+			Base::vector EscClassic::gradientEstimation(numericprecission cost_signal_filtered, vector additional_input){
+
+				vector signal_demodulated(controlNum);
+				//for (size_t i = 0; i<controlNum; i++){
+				//	signal_demodulated[i]= cost_signal_filtered*sin_amp_[i]*std::sin(double(cycle_count_*period_*sin_freq_[i] + phase_shift_[i]));
+				//}
+				return signal_demodulated;
+			}
+
+			Base::vector EscClassic::postFiltering(vector estimated_gradient){
+
+				vector lpf_out(controlNum);
+				vector comp_out(controlNum);
+
+				for (size_t i = 0; i<controlNum; i++){
+
+					lpf_out[i] = ((2.0-low_pass_pole_[i]*period_)*lpf_out_old_[i]+low_pass_pole_[i]*period_*estimated_gradient[i]+low_pass_pole_[i]*period_*estimated_gradient_old_[i])/(2.0+low_pass_pole_[i]*period_);
+					comp_out[i]= ((2.0+period_*comp_zero_[i])*lpf_out[i]+(period_*comp_zero_[i]-2.0)*lpf_out_old_[i]-(period_*comp_pole_[i]-2.0)*comp_old_[i])/(2.0+period_*comp_pole_[i]);
+					corr_signal_[i] = corr_signal_[i]+corr_gain_[i]*comp_out[i]*period_;
+
+					lpf_out_old_[i] = lpf_out[i];
+					comp_old_[i] = comp_out[i];
+				}
+
+				return corr_signal_;
+
+			}
+
+			Base::vector EscClassic::superimposePerturbation(Base::vector control){
+
+				for (size_t i = 0; i<controlNum; i++){
+					if(!old_vals_initialized_)
+						control_ref_[i] = sin_amp_[i]*std::sin(double(cycle_count_*period_*sin_freq_[i] + phase_shift_[i]));
+					else
+						control_ref_[i] = control[i]+sin_amp_[i]*std::sin(double(cycle_count_*period_*sin_freq_[i] + phase_shift_[i]));
+				}
+
+				return control_ref_;
+			}
 		}
-
-		phase_shift_.resize(opt_dim_);
-		phase_shift_[0] = 0;
-		for (size_t i = 1; i<opt_dim_; i++){
-			phase_shift_[i] = i*PI/((double)opt_dim_);
-		}
-		state_initialized_ = true;
 	}
-
-	double hpf_out = (-(period_*high_pass_pole_-2)*hpf_out_old_+2*obj_val-2*obj_val_old_)/(2+high_pass_pole_*period_);
-	hpf_out_old_ = hpf_out;
-	obj_val_old_ = obj_val;
-	std::vector<double> signal_demodulated(opt_dim_);
-	std::vector<double> lpf_out(opt_dim_);
-	std::vector<double> comp_out(opt_dim_);
-	std::vector<double> output;
-
-	for (size_t i = 0; i<opt_dim_; i++){
-		signal_demodulated[i]= hpf_out*sin_amp_*std::sin(cycle_count_*period_*sin_freq_ + phase_shift_[i]);
-		lpf_out[i] = ((2.0-low_pass_pole_*period_)*lpf_out_old_[i]+low_pass_pole_*period_*signal_demodulated[i]+low_pass_pole_*period_*signal_demodulated_old_[i])/(2.0+low_pass_pole_*period_);
-		comp_out[i]= ((2.0+period_*comp_zero_)*lpf_out[i]+(period_*comp_zero_-2.0)*lpf_out_old_[i]-(period_*comp_pole_-2.0)*comp_old_[i])/(2.0+period_*comp_pole_);
-		corr_signal_[i] = corr_signal_[i]+corr_gain_*comp_out[i]*period_;
-		if(!old_vals_initialized_)
-			pos_ref_[i] = sin_amp_*std::sin(cycle_count_*period_*sin_freq_ + phase_shift_[i]);	//modulation
-		else
-			pos_ref_[i] = corr_signal_[i]+sin_amp_*std::sin(cycle_count_*period_*sin_freq_ + phase_shift_[i]);	//modulation
-		output.push_back(pos_ref_[i]);
-		signal_demodulated_old_[i] = signal_demodulated[i];
-		lpf_out_old_[i] = lpf_out[i];
-		comp_old_[i] = comp_out[i];
-	}
-	old_vals_initialized_ = true;
-	cycle_count_++;
-
-	return output;
 }
 
-ESC::inputType PerturbESCND::getInputType(){
-	return ESC::inputStateValue;
+using namespace labust::control::esc;
+
+int main(int argc, char** argv){
+
+	ros::init(argc, argv, "ESCclassic");
+	ros::NodeHandle nh;
+
+	EscClassic ESC;
+
+
+	ros::spin();
+
+	return 0;
 }
 
-ESC::outputType PerturbESCND::getOutputType(){
-	return ESC::outputPosition;
-}
 
-std::vector<double> PerturbESCND::monitor(){
-	std::vector<double> monitor_values;
 
-	for(size_t i = 0; i<opt_dim_; i++)
-		monitor_values.push_back(corr_signal_[i]);
 
-	return monitor_values;
-}
-std::vector<std::string> PerturbESCND::monitorNames(){
-	std::vector<std::string> monitor_names;
-	std::string base = "correction signal ";
-	char numstr[21];
-	for(size_t i = 0; i<opt_dim_; i++){
-		sprintf(numstr, "%zd", i+1);
-		monitor_names.push_back(base + numstr);
-	}
-
-	return monitor_names;
-}
-
-void PerturbESCND::reset(){
-	obj_val_old_ = 0;
-	cycle_count_ = 0;
-	hpf_out_old_ = 0;
-	opt_dim_ = 0;
-	state_initialized_ = false;
-	old_vals_initialized_ = false;
-}
